2522d04214c41ce29c602831692e471bfb221c16
xianghui <xianghui@iwgame.com>
2016-04-11 09:49:18 +0800

    Initial Commit

---
+++ iOSTests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSTests
+//
+//  Created by xianghui on 16/4/11.
+//  Copyright © 2016年 xh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSTests/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSTests
+//
+//  Created by xianghui on 16/4/11.
+//  Copyright © 2016年 xh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
